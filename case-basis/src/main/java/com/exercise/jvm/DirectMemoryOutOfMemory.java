a89c06b3c127ea1882ef54ebcc3d146a7e6dfdde
liuguofeng <liuguofeng123>
2017-09-05 18:08:59 +0800

    gc

--- case-basis/src/main/java/com/exercise/jvm/DirectMemoryOutOfMemory.java
+++ case-basis/src/main/java/com/exercise/jvm/DirectMemoryOutOfMemory.java
@@ -22,20 +22,20 @@ import java.nio.ByteBuffer;
  * @createtime 2017/9/5 下午4:25
  * @see jdk 1.7
  * -verbose:gc  -Xms48M -Xmx48M -XX:MaxDirectMemorySize=5M -Xss1M -XX:+PrintGCDetails
- * -Xmx   Java Heap最大值，默认值为物理内存的1/4，最佳设值应该视物理内存大小及计算机内其他内存开销而定；
- * -Xms   java Heap初始值，Server端JVM最好将-Xms和-Xmx设为相同值，开发测试机JVM可以保留默认值；
- * -Xmn   Java Heap Young区大小，不熟悉最好保留默认值；
- * -Xss   每个线程的Stack大小，不熟悉最好保留默认值；
+ * -Xmx Java Heap最大值，默认值为物理内存的1/4，最佳设值应该视物理内存大小及计算机内其他内存开销而定；
+ * -Xms java Heap初始值，Server端JVM最好将-Xms和-Xmx设为相同值，开发测试机JVM可以保留默认值；
+ * -Xmn Java Heap Young区大小，不熟悉最好保留默认值；
+ * -Xss 每个线程的Stack大小，不熟悉最好保留默认值；
  **/
 public class DirectMemoryOutOfMemory {
-    public static final int ONE_G = 1024 * 1024 * 1024;
+    public static final int ONE_GB = 1024 * 1024 * 1024;
 
     // Direct buffer memory
     public static void main(String[] args) {
         int i = 0;
         try {
             while (true) {
-                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(ONE_G);
+                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(ONE_GB);
                 i++;
             }
         } catch (Exception e) {
--- case-basis/src/main/java/com/exercise/jvm/StackOverFlowOutOfMemory.java
+++ case-basis/src/main/java/com/exercise/jvm/StackOverFlowOutOfMemory.java
@@ -14,11 +14,12 @@ public class StackOverFlowOutOfMemory {
     int i = 0;
 
     public void count(){
-        i ++;
+        i++;
         count();
     }
 
-    //栈溢出，stack区是线程私有
+    //栈溢出，stack区是线程私有，每个线程都包含一个栈区域，Stack区包括基本数据类型与对象的引用，其他线程不能访问
+    //stack 分为三部分，基本数据类型区，操作指令区，上下文等，
     public static void main(String[] args) {
         try {
             StackOverFlowOutOfMemory stackOverFlow = new StackOverFlowOutOfMemory();
