c6a8e51ae3c86b63f707dd13cc0eda0b8c5964eb
liuguofeng <liuguofeng123>
2018-12-25 10:54:33 +0800

    测试CountDownLatch

---
+++ case-basis/src/main/java/com/exercise/RunTimeDemo.java
@@ -0,0 +1,96 @@
+package com.exercise;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author guofeng
+ * @version 1.0
+ * @desc
+ * @createtime 2018/12/25 9:53 AM
+ * @see jdk 1.7
+ **/
+public class RunTimeDemo {
+
+    private CountDownLatch count = new CountDownLatch(5);
+    CopyOnWriteArrayList<Integer> copy = new CopyOnWriteArrayList<>();
+    ThreadLocal<Integer> local = new ThreadLocal<Integer>() {
+        @Override
+        protected Integer initialValue() {
+            return 0;
+        }
+    };
+
+    AtomicInteger atomic = new AtomicInteger();
+
+    private void testCountDownLatch() {
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                try {
+                    for (int j = 0; j < 3; j++) {
+                        local.set(local.get() + 1);
+                        System.out.println(local.get() + "====" + Thread.currentThread().getName());
+                    }
+                    copy.add(local.get());
+                    TimeUnit.MICROSECONDS.sleep(1000);
+                    final int get = atomic.incrementAndGet();
+                    if (get % 2 == 0) {
+                        int i1 = 1 / 0;
+                    }
+                    count.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+        try {
+            count.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int total = 0;
+        for (int i = 0; i < copy.size(); i++) {
+            total += copy.get(i).intValue();
+        }
+
+        System.out.println("每个线程的计算的总和 = " + total);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("正在释放资源");
+            }
+        }, "ShutdownHook"));
+
+
+    }
+
+    public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler(new CrashHandler());
+        RunTimeDemo runTimeDemo = new RunTimeDemo();
+        runTimeDemo.testCountDownLatch();
+    }
+}
+
+class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        Throwable cause = e.getCause();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream s = new PrintStream(outputStream);
+        e.printStackTrace(s);
+        final byte[] bytes = outputStream.toByteArray();
+        while (cause != null) {
+            e.printStackTrace(s);
+            cause = e.getCause();
+        }
+
+        System.out.println(t.getName() + " = " + new String(bytes));
+    }
+}
