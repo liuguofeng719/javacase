5ea513a1e5541fd6f9eaf8891ea27a9a41222ca2
liuguofeng <liuguofeng123>
2019-02-05 20:38:46 +0800

    增加 spring-boot2x 整合jsp jar 运行和war 运行

---
+++ spring-boot2x/src/main/java/com/springboot2x/Springboot2xApplication.java
@@ -0,0 +1,12 @@
+package com.springboot2x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2xApplication  {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2xApplication.class, args);
+    }
+}
+
